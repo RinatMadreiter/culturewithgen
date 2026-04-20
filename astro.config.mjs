617aa909e04bd3675fbb3c3ccc704772aaab9116
RinatMadreiter <95545985+RinatMadreiter@users.noreply.github.com>
2026-04-20 19:21:54 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://culturewithgen.com',
+  integrations: [sitemap()],
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    routing: {
+      prefixDefaultLocale: false, // English stays at '/'
+    }
+  }
+});
